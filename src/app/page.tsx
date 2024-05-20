17d37a9f0d39c7411f26e4f1ad40d77eb8ec66d3
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-05-20 17:06:33 +0300

    feat: add date range picker in sub dropdown menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,49 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+import { addDays } from "date-fns";
+import { DateRange } from "react-day-picker";
+
 export default function Home() {
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: new Date(2022, 0, 20),
+    to: addDays(new Date(2022, 0, 20), 20),
+  });
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      main
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Open</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>Custom Range</DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <Calendar
+                  initialFocus
+                  mode="range"
+                  defaultMonth={date?.from}
+                  selected={date}
+                  onSelect={setDate}
+                  numberOfMonths={2}
+                />
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </main>
   );
 }
